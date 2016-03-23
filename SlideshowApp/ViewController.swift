18cc0bdb38b975293efaf717aea6f30c4d590827
taku00jp <saitou@saitou-no-imac.local>
2016-03-23 19:03:09 +0900

    修正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,43 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nextbtn: UIButton!
+    @IBOutlet weak var backbtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //UIImageに画像の名前を指定します
         let img1 = UIImage(named:"img1.jpg");
         let img2 = UIImage(named:"img2.jpg");
         let img3 = UIImage(named:"img3.jpg");
         
-  
+        //UIImageViewにUIIimageを追加
         let imageView1 = UIImageView(image:img1)
         let imageView2 = UIImageView(image:img2)
         let imageView3 = UIImageView(image:img3)
         
+        //UIScrollViewを作成します
         let scrView = UIScrollView()
         
-
-        scrView.frame = CGRectMake(100, 100, 240, 240)
+        //表示位置 + 1ページ分のサイズ
+        scrView.frame = CGRectMake(50, 50, 240, 240)
         
+        //全体のサイズ
         scrView.contentSize = CGSizeMake(240*3, 240)
         
-  
+        //左右に並べる
         imageView1.frame = CGRectMake(0, 0, 240, 240)
         imageView2.frame = CGRectMake(240, 0, 240, 240)
         imageView3.frame = CGRectMake(480, 0, 240, 240)
-   
+        
+        
+        //viewに追加
         self.view.addSubview(scrView)
         scrView.addSubview(imageView1)
         scrView.addSubview(imageView2)
         scrView.addSubview(imageView3)
         
-    
+        // １ページ単位でスクロールさせる
         scrView.pagingEnabled = true
         
+        
+        //scroll画面の初期位置
+        scrView.contentOffset = CGPointMake(0, 0);
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
     }
+    @IBAction func nextbtn(sender: UIButton) {
+
+    
+    }
+  
+    
+    @IBAction func backbtn(sender: UIButton) {
+        
+        
+    }
+    
 }
+
